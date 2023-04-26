3a77a487ae8f65f983cadf44d7fc4e127bdb2b95
Sanskriti <123055093+Sanskriti-56@users.noreply.github.com>
2023-04-26 14:04:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,15 @@
 import streamlit as st
-# generate the dataset
+import numpy as np
 from sklearn.datasets import make_classification
 from sklearn.feature_selection import SequentialFeatureSelector
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
-#%matplotlib inline
 import ipywidgets as widgets
 from ipywidgets import interact, IntSlider
 from IPython.display import display
-import time
-
-
-
+from joblib import Parallel, delayed
+from tqdm import tqdm
 
 
 Accuracycount={}
@@ -20,78 +17,45 @@ st.title("Logistic Regression")
 st.subheader("Forward and Backward Feature Selection")
 st.header("Machine Learning")
 st.markdown("---  ")
-#st.text("hello world")
-
-#st.markdown("*hello* World ")
-
-
-
-bar = st.progress(0)
-
-
 
 X, y = make_classification(n_samples=100, n_features=20, n_informative=15,
                            n_redundant=2, n_repeated=0, n_classes=2,
                            class_sep=2.0, random_state=42)
 
-# create a sequential forward feature selector
 sfs = SequentialFeatureSelector(LogisticRegression(), direction='forward', n_features_to_select=10)
 
-# create a sequential backward feature selector
 sbs = SequentialFeatureSelector(LogisticRegression(), direction='backward', n_features_to_select=10)
 
-# initialize empty lists to store the number of selected features and corresponding scores
-n_features_fwd = []
-scores_fwd = []
-n_features_bwd = []
-scores_bwd = []
+n_features_fwd = np.zeros(2)
+scores_fwd = np.zeros(2)
+n_features_bwd = np.zeros(2)
+scores_bwd = np.zeros(2)
 
 st.write('Starting a long computation...')
-# loop through a range of iterations to select a variable number of features
-for i in range(1, 3):
+for i in tqdm(range(1, 3)):
 
+    def fit_sfs(X, y, i):
+        sfs = SequentialFeatureSelector(LogisticRegression(), direction='forward', n_features_to_select=i)
+        selected_features_fwd = sfs.fit_transform(X, y)
+        lr = LogisticRegression().fit(selected_features_fwd, y)
+        score_fwd = accuracy_score(y, lr.predict(selected_features_fwd))
+        return score_fwd
 
+    def fit_sbs(X, y, i):
+        sbs = SequentialFeatureSelector(LogisticRegression(), direction='backward', n_features_to_select=i)
+        selected_features_bwd = sbs.fit_transform(X, y)
+        lr = LogisticRegression().fit(selected_features_bwd, y)
+        score_bwd = accuracy_score(y, lr.predict(selected_features_bwd))
+        return score_bwd
 
-    # create a new instance of the logistic regression model
-    lr = LogisticRegression()
-    sfs = SequentialFeatureSelector(LogisticRegression(), direction='forward', n_features_to_select=i) 
-    # fit the sequential forward feature selector
-    sfs.fit(X, y)
-    # get the selected features and their corresponding scores
-    selected_features_fwd = sfs.transform(X)
-    lr.fit(selected_features_fwd, y)
-    score_fwd = accuracy_score(y, lr.predict(selected_features_fwd))
-    # append the number of selected features and the corresponding score to the lists
-    n_features_fwd.append(i)
-    scores_fwd.append(score_fwd)
-    st.write(score_fwd)
-
-
-    
-    #time.sleep(0.05)
-
-
-    # create a new instance of the logistic regression model
-    lr = LogisticRegression()
-    sbs = SequentialFeatureSelector(LogisticRegression(), direction='backward', n_features_to_select=i)
-    # fit the sequential backward feature selector
-    sbs.fit(X, y)
-    # get the selected features and their corresponding scores
-    selected_features_bwd = sbs.transform(X)
-    lr.fit(selected_features_bwd, y)
-    score_bwd = accuracy_score(y, lr.predict(selected_features_bwd))
-    # append the number of selected features and the corresponding score to the lists
-    n_features_bwd.append(i)
-    scores_bwd.append(score_bwd)
-    st.write(score_bwd)
-
-    bar.progress(i+20)
+    scores_fwd[i-1] = fit_sfs(X, y, i)
+    scores_bwd[i-1] = fit_sbs(X, y, i)
+    n_features_fwd[i-1] = i
+    n_features_bwd[i-1] = i
 
 st.balloons()
-#st.write('...and now we\'re done!')
 st.success('Done!')
 
-# define a function to plot the scores
 def plot_scores(n):
     fig=plt.figure()
     plt.style.use("https://github.com/dhaitz/matplotlib-stylesheets/raw/master/pitayasmoothie-dark.mplstyle")
@@ -101,14 +65,4 @@ def plot_scores(n):
     plt.ylabel('Accuracy')
     plt.title('Sequential Feature Selection')
     plt.legend()
-    #plt.show()
-    st.pyplot(fig)
-    
-if _name_ == '_main_':
-    #run_app()   
-
-# create a slider for the number of iterations
-    iterations_slider = st.slider(label="Iterations", min_value=1, max_value=len(n_features_fwd), value=len(n_features_fwd))
-
-# use the interact function to link the slider to the plot
-    interact(plot_scores, n=iterations_slider)
+   
